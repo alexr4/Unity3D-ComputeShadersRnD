33dfe909c75e834dab4943dbb41825528782da7c
ALEX-MSI\bonjour <alex@bonjour-lab.com>
2019-08-19 10:06:22 +0200

    no message

---
+++ Assets/Tuts-02/CSBaseController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CSBaseController : MonoBehaviour
+{
+    public ComputeShader shader;
+    RenderTexture tex;
+    int kernelHandle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        RunShader();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shader.SetFloat("time", (float)Time.time);
+        shader.Dispatch(kernelHandle, 256/8, 256/8, 1);
+    }
+
+
+    void RunShader()
+    {
+        kernelHandle = shader.FindKernel("CSMain");
+
+        tex = new RenderTexture(256,256,24);//8*8*32*32 → total amount of pixels
+        tex.enableRandomWrite = true;
+        tex.Create();
+
+        shader.SetTexture(kernelHandle, "Result", tex);
+    }
+
+     void OnGUI(){
+        int textureSize = 256;
+        GUI.DrawTexture(new Rect(0, 0, textureSize, textureSize), tex);
+    }
+}
---
+++ Assets/Tuts-02/T05CSController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class T05CSController : MonoBehaviour
+{
+    struct VecMatPair{
+        public Vector3 point;
+        public Matrix4x4 matrix;
+    }
+ 
+    public ComputeShader shader;
+     ComputeBuffer buffer;
+    VecMatPair[] data;
+    int kernel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        RunShader();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shader.Dispatch(kernel, data.Length, 1, 1);
+        Output();
+    }
+
+
+    void RunShader()
+    {
+        data = new VecMatPair[5];
+        for(int i=0; i<data.Length; i++){
+            Matrix4x4 mat = Matrix4x4.identity;
+            Vector3 point = Random.insideUnitSphere;
+            VecMatPair vmp = new VecMatPair();
+            vmp.point = point;
+            vmp.matrix = mat;
+            data[i] = vmp;
+        }
+
+        buffer = new ComputeBuffer(data.Length, (3 + 4 * 4) * sizeof(float));
+        buffer.SetData(data);
+
+        kernel = shader.FindKernel("Multiply");
+        shader.SetBuffer(kernel, "dataBuffer", buffer);
+    }
+
+    void Output(){
+        VecMatPair[] output = new VecMatPair[5];
+        buffer.GetData(output);
+
+        for(int i=0; i<output.Length; i++){
+            Debug.Log(i+": "+output[i].point);
+        }
+    }
+    
+    void OnDestroy(){
+        //Delet buffer after simulation
+        buffer.Dispose();
+    }
+}
